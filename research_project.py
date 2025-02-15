0375bc8accfbf18e1f1da2f4a0f149ae9e49f95b
HARMONSC21 <harmonsc21@gcc.edu>
2025-02-14 20:10:22 -0500

    main now correctly reads from files

--- research_project.py
+++ research_project.py
@@ -6,9 +6,19 @@ class Time_Block:
         self.end_time = end_time
         self.day = day
 
+    def __str__(self):
+        return f"{self.day}: {self.start_time} - {self.end_time}"
+
 class Student:
-    def __init__(self, unavailable_times: list[Time_Block]):
+    def __init__(self, name: str, unavailable_times: list[Time_Block]):
+        self.name = name
         self.unavailable_times = unavailable_times
+
+    def __str__(self):
+        ret_str = f"{self.name}:\n"
+        for time in self.unavailable_times:
+            ret_str += f"{time}\n"
+        return ret_str
 
 class MTA:
     def __init__(self, students: list[Student], type, times: list[Time_Block], length):
@@ -17,29 +27,55 @@ class MTA:
         self.times = times
         self.length = length
 
+    def __str__(self):
+        ret_str = f"{self.type}:\n"
+        for student in self.students:
+            ret_str += f"{student}\n"
+        ret_str += f"Length: {self.length}\n"
+        for time in self.times:
+            ret_str += f"{time}\n"
+        return ret_str
+
 def main():
     mtas_df = pd.read_excel("DATA/mtas.xlsx")
     mta_type_df = pd.read_excel("DATA/mta_type_availability.xlsx")
     student_df = pd.read_excel("DATA/student_unavailability.xlsx")
-    mta_list = []
-    mtas = mtas_df.iterrows()
-    #print(mtas_df)
-    print(student_df)
-    students = {}
+    student_times = {}
     for row in student_df.iterrows():
         data = row[1]
         student = data["Student Name"]
-        print(student == "NaN")
-        if student != "nan":
+        if isinstance(student, str):
             curr_student = student
         try:
-            students[curr_student].append(Time_Block(data["Unavailability Start Time"], data["Unavailability End Time"], data["Unavailability Day"]))
+            student_times[curr_student].append(Time_Block(data["Unavailability Start Time"], data["Unavailability End Time"], data["Unavailability Day"]))
         except:
-            students[curr_student] = [Time_Block(data["Unavailability Start Time"], data["Unavailability End Time"], data["Unavailability Day"])]
-    print(students)
+            student_times[curr_student] = [Time_Block(data["Unavailability Start Time"], data["Unavailability End Time"], data["Unavailability Day"])]
+    students = {}
+    for key in student_times.keys():
+        students[key] = (Student(key, student_times[key]))
+    mta_times = {}
+    for row in mta_type_df.iterrows():
+        data = row[1]
+        mta_type = data["Type"]
+        if isinstance(mta_type, str):
+            curr_type = mta_type
+        try:
+            mta_times[curr_type].append(Time_Block(data["Availability Start Time"], data["Availability End Time"], data["Availability Day"]))
+        except:
+            mta_times[curr_type] = [Time_Block(data["Availability Start Time"], data["Availability End Time"], data["Availability Day"])]
+    mtas = []
     for row in mtas_df.iterrows():
-        mta_type = row[1]["Type"]
-         
+        data = row[1]
+        students_str = data["Students"].split(", ")
+        student_list = []
+        for student in students_str:
+            student_list.append(students[student])
+        mta_type = data["Type"]
+        mtas.append(MTA(student_list, mta_type, mta_times[mta_type], data["Length (minutes)"]))
+    print(mtas[1])
+
+
+
 
 if __name__=="__main__":
     main()
