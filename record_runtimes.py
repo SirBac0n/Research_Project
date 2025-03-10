05fbc46b9ef2b570a0b4ba4b6cdf5b25760ac281
HARMONSC21 <harmonsc21@gcc.edu>
2025-03-09 22:00:06 -0400

    started plotting

--- record_runtimes.py
+++ record_runtimes.py
@@ -3,9 +3,12 @@ from backtrack import backtrack
 from MTA_Solver import read_mtas
 from domain_helper import get_domains
 from problem_instances import generate_problem_instances
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def main():
+    """df = pd.DataFrame(columns=["Algorithm", "Num_MTAs", "Runtime", "Result"])
     # Always make 10 students
     num_students = 10
     # Start with 1 MTA and go to 20 MTA's
@@ -33,6 +36,8 @@ def main():
             backtrack_time += (end_time - start_time)
             backtrack_successes += 1 if result else 0
 
+            df.loc[len(df)] = ["Backtracking", num_mtas, (end_time - start_time)*1000, 1 if result else 0]
+
             # Load MTA's and domains
             mtas = read_mtas()
             domains = get_domains(mtas)
@@ -44,6 +49,8 @@ def main():
             remembering_no_goods_time += (end_time - start_time)
             remembering_no_goods_successes += 1 if result else 0
 
+            df.loc[len(df)] = ["Remembering No Goods", num_mtas, (end_time - start_time)*1000, 1 if result else 0]
+
             if backtrack_successes != remembering_no_goods_successes:
                 raise ValueError("Successes should be the same!")
 
@@ -53,6 +60,13 @@ def main():
 
         print(f"\tBacktracking: {backtrack_successes} / {num_runs} succeeded with average time of {backtrack_time*1000} milliseconds")
         print(f"\tRemembering No-Goods: {remembering_no_goods_successes} / {num_runs} succeeded with average time of {remembering_no_goods_time*1000} milliseconds")
+"""
+    #df.to_csv("temp.xlsx")
+    df = pd.read_csv("temp.csv")
+    fig, ax = plt.subplots(figsize=(8, 6))
+    df.plot(kind="scatter", x="Num_MTAs", y="Runtime", ax=ax)
+    df.plot(kind="line", x="Num_MTAs", y="Runtime", ax=ax)
+    plt.show()
 
 if __name__ == '__main__':
     main()
